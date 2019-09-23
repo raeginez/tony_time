b38b6fc534eba718e883acad6dcf74d78fc07d02
BlueCodeThree <carliemhamilton@gmail.com>
2019-09-23 20:21:05 +1000

    fixes .catch and gets the data

--- script.js
+++ script.js
@@ -5,24 +5,34 @@ const button = document.querySelector('.button');
 // asynchronous function to get the JSON document from the url
 function getJSON() {
     fetch(url).then(response => response.json())
-        .then(data => showCocktail(data));
-    // .catch (e => {
-    // cocktail.innerHTML = "<h3>Oooops ... there was a problem!</h3>";
-    // console.error(e);
-    // });
+        .then(data => showCocktail(data))
+        .catch (e => {
+        cocktail.innerHTML = "<h3>Oooops ... there was a problem!</h3>";
+        console.error(e);
+        });
 }
-getJSON();
+// getJSON();
 
 // function to display data in browser
 function showCocktail(data) {
-    console.log(data.drinks);
+    let drink = data.drinks[0];
+    console.log(drink);
     const card = document.createElement('div');
     card.className = 'card';
     cocktail.appendChild(card);
+    // we can add in an image - the url is drink.strDrinkThumb
+    cocktail.innerHTML = "Name: " + drink.strDrink + "<br />";
+    cocktail.innerHTML += "Ingredients: <br />"
+    // could do string interpolation? Also, we could make this into a list with html.
+    cocktail.innerHTML += drink.strMeasure1 + drink.strIngredient1 + "<br />";
+    cocktail.innerHTML += drink.strMeasure2 + drink.strIngredient2 + "<br />";
+    cocktail.innerHTML += drink.strMeasure3 + drink.strIngredient3 + "<br />";
+    cocktail.innerHTML += drink.strMeasure4 + drink.strIngredient4 + "<br />";
+    cocktail.innerHTML += drink.strMeasure5 + drink.strIngredient5 + "<br /><br />";
+    cocktail.innerHTML += "Instructions: <br />" + drink.strInstructions;
 }
 
 button.addEventListener('click', (e) => {
     e.preventDefault();
-    cocktail.innerHTML = "";
-    getJSON;
+    getJSON();
 });
